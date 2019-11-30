fde62061b19ff61ee60f70a659d80d021e7b9035
chirayu-sanghvi <54971056+chirayu-sanghvi@users.noreply.github.com>
2019-11-30 14:08:46 +0530

    Create changeString.py

---
+++ python/changeString.py
@@ -0,0 +1,16 @@
+def convertString(st):
+	st=list(st)
+
+	for i in range(0,len(st)):
+		if (st[i].isalpha()):
+			if(st[i] == 'Z'):
+				st[i]='a'
+			else:
+				st[i]=chr(ord(st[i])+1)
+
+	for i in range(0,len(st)):
+		if st[i] in ['a','e','i','o','u']:
+			st[i]=st[i].upper()
+	print("".join(st))
+
+convertString("hzZlo*3")
